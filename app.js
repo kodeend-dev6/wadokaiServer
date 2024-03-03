2c51ce0fda3fecf62186eb13b9e89ffca4860843
kodeend-dev6 <kodeend.dev6@gmail.com>
2024-03-03 13:02:37 +0600

    updated the server

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const { errorResponse } = require("./Controllers/responseController");
 const danGradeRouter = require("./Routes/danGradeRouter");
 const countryListRouter = require("./Routes/countryListRouter");
 const memberRouter = require("./Routes/memberRouter");
+const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+connectDB();
 
 app.use("/api/dan-grades", danGradeRouter);
 app.use("/api/country-list", countryListRouter);
--- server.js
+++ server.js
@@ -1,8 +1,47 @@
-const app = require('./app');
-const connectDB = require('./config/db');
-const { port } = require('./secret');
+/* eslint-disable no-undef */
+const app = require("./app");
+const { port } = require("./secret") || 4000;
 
-app.listen(port, async() => {
-    await connectDB();
-    console.log(`Wadokai is running at http://localhost:${port}`);
+// Uncaught exception
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION! 💣 Shutting down...");
+  console.error(err.message);
+  process.exit(1);
+});
+
+let server;
+// Run the server
+const startServer = async () => {
+  try {
+    // Start the server
+    server = app.listen(port, () => {
+      console.log(`Server listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  // Unhandled promise rejection
+  process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+    console.log(err.message);
+    if (server) {
+      server.close(() => {
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+};
+
+// Run the server
+startServer();
+
+// SIGTERM
+process.on("SIGTERM", () => {
+  console.info("SIGTERM RECEIVED 🚦 Shutting down gracefully");
+  if (server) {
+    server.close();
+  }
 });
